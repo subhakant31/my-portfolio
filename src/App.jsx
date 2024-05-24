cf83aea12303c413729c3e8688b45ba5893318d0
subhakant31 <subhakanta.mishra3105@gmail.com>
2024-05-24 21:01:20 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+import { RouterProvider } from "react-router-dom";
+function App() {
+  return (
+    <RouterProvider >
+
+    </RouterProvider>
+  );
+}
+
+export default App;
